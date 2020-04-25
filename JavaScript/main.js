a04e13b7bee6740e0115b712ab3ac5ea754a953a
Isaac Hillaker <hillakerisaac@gmail.com>
2020-04-25 14:10:08 -0500

    Created smooth scroll for contact information.

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,10 +1,21 @@
-// Animate Smooth Scroll
+// Animate Smooth Scroll for 'My Work'
 $('#view-gallery').on('click', function() {
   const images = $('#images').position().top;
 
   $('html, body').animate(
     {
       scrollTop: images
+    },
+    900
+  );
+});
+// Smooth scroll animation for 'contact info'
+$('#view-contact').on('click', function() {
+  const contactInfo = $('footer').position().top;
+
+  $('html, body').animate(
+    {
+      scrollTop: contactInfo
     },
     900
   );
